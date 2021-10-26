44895931c9af7525576af76036b021d979895450
JakariaPUST <jakaria.pust@gmail.com>
2021-10-26 09:57:41 +0600

    template added and html page tested

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,7 @@
+from django.views.generic import TemplateView
 from django.shortcuts import render
 from django.http import HttpResponse
 
+class HomePageView(TemplateView):
 
-def home_page_view(request):
-    return HttpResponse('Hello, World!')
+    template_name = 'home.html'
