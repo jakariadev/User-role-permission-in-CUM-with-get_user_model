bc039932d9f799038bab0b40f3017addd96d7b94
JakariaPUST <jakaria.pust@gmail.com>
2021-10-26 13:09:09 +0600

    About section added

--- pages/views.py
+++ pages/views.py
@@ -3,5 +3,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 class HomePageView(TemplateView):
-
     template_name = 'home.html'
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
