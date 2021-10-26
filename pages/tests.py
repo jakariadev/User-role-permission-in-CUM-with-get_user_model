8a4f100e8abd22e15c73683a1b05c167dc3fe84a
JakariaPUST <jakaria.pust@gmail.com>
2021-10-26 10:05:51 +0600

    Unit testing for html contents

--- pages/tests.py
+++ pages/tests.py
@@ -15,3 +15,15 @@ class HomepageTests(SimpleTestCase):
     def test_homepage_template(self):  # new
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+
+        
+    # For html contents
+    def test_homepage_contains_correct_html(self):  
+        response = self.client.get('/')
+        self.assertContains(response, 'Homepage')
+
+    # For html contents
+    def test_homepage_does_not_contain_incorrect_html(self):
+        response = self.client.get('/')
+        self.assertNotContains(
+            response, 'Hi there! I should not be on the page.')
