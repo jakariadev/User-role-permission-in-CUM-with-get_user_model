760f74fb49b6cfb3675f3656e0c94415b7fcc4fd
JakariaPUST <jakaria.pust@gmail.com>
2021-10-27 14:09:54 +0600

    model name precised

--- accounts/models.py
+++ accounts/models.py
@@ -6,10 +6,19 @@ from django.db import models
 class Permissions(models.Model):
     name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
+
 class Role(models.Model):
     name = models.CharField(max_length=200)
     permissions = models.ManyToManyField(Permissions)
 
+    def __str__(self):
+        return self.name
+
 class CustomUser(AbstractUser):
     role = models.ForeignKey(Role, blank=True, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.username
 
