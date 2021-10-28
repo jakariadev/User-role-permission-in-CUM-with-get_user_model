f9d21c4d60f1adab8e45e4812d7748e5cc14c55d
JakariaPUST <jakaria.pust@gmail.com>
2021-10-28 13:57:04 +0600

    Custom tag for User Group,used in templates and checked in template file

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Books
 
+admin.site.register(Books)
 # Register your models here.
--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Books(models.Model):
+    title = models.CharField(max_length=200)
+    author_name = models.CharField(max_length=200)
+    pageno= models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
