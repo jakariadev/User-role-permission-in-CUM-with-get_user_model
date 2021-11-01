ad5eaf4a894d6a28ef66393578c92b7c50d93d61
JakariaPUST <jakaria.pust@gmail.com>
2021-11-01 10:09:08 +0600

    Register all models in django admin by one python code

--- pages/admin.py
+++ pages/admin.py
@@ -1,5 +1,29 @@
-from django.contrib import admin
-from .models import Books
+# from django.contrib import admin
+# from .models import Books
 
-admin.site.register(Books)
-# Register your models here.
+# admin.site.register(Books)
+# # Register your models here.
+
+from django.apps import apps
+from django.contrib import admin
+from django.contrib.admin.models import LogEntry
+from django.contrib.auth.models import *
+from django.contrib.sessions.models import Session
+
+class ListAdminMixin(object):
+    def __init__(self, model, admin_site):
+        self.list_display = [field.name for field in model._meta.fields]
+        super(ListAdminMixin, self).__init__(model, admin_site)
+
+models = apps.get_models()
+
+for model in models:
+    admin_class = type('AdminClass', (ListAdminMixin, admin.ModelAdmin), {})
+    try:
+        admin.site.register(model, admin_class)
+    except admin.sites.AlreadyRegistered:
+        pass
+
+admin.site.unregister(ContentType)
+admin.site.unregister(LogEntry)
+admin.site.unregister(Session)
